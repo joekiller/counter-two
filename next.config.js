7903f0b019fd7736410f1896ad27628e0aad0f8e
Joseph Lawson <joe@joekiller.com>
2024-01-01 12:01:59 -0500

    build: use static exports
    https://nextjs.org/docs/app/building-your-application/deploying/static-exports

--- next.config.js
+++ next.config.js
@@ -9,7 +9,8 @@ const nextConfig = {
     NEXT_PUBLIC_PACKAGE_VERSION: typeof version === "string" ? version : 'unknown',
     NEXT_PUBLIC_LAST_UPDATED_AT: lastUpdatedAt,
     NEXT_PUBLIC_COUNTS_UPDATED_AT: new Date(1703392020).toString(),
-  }
+  },
+  output: 'export'
 }
 
 module.exports = nextConfig
